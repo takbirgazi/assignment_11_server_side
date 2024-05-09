0761831e18bf4c5d87d1dcf34233892ce80a1d5a
takbirgazi <takbirgazibd@gmail.com>
2024-05-10 00:02:36 +0600

    basic setup done

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use((req, res)=>{
+    res.send("This is home page");
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running at ${port}`);
+});
