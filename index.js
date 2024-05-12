dfc686b117848408405489daed9ae95a42e022ec
takbirgazi <takbirgazibd@gmail.com>
2024-05-12 21:04:04 +0600

    add vercel

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
+app.use(express.json())
 
-//
+// MongoDB Start
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h4cvmus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-//
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("sevenStarDB"); // Database Name
+    const users = database.collection("user"); // Collection or Table Name
+
+    app.post('/user', async(req, res)=>{
+      const usr = req.body;
+      const usrData = await users.insertOne(usr);
+      res.send(usrData);
+    })
+    app.get('/user', async(req, res)=>{
+      const usr = users.find();
+      const result = await usr.toArray();
+      res.send(result);
+    })
 
 
-app.use((req, res)=>{
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// MongoDB End
+
+
+app.get('/',(req, res)=>{
     res.send("This is home page");
 })
 
